c86d7a7702953bc1a459826122d5723d82276633
Saegor <saegor@pm.me>
2023-02-20 09:43:23 +0000

    Add files via upload
    Initial upload of a pre-existing project

---
+++ Main.gd
@@ -0,0 +1,35 @@
+extends Node
+
+
+var Note: PackedScene = load("res://Note.tscn")
+
+
+func _unhandled_input(event):
+
+    if event is InputEventScreenTouch or event is InputEventScreenDrag:
+
+        var note_name: String = "NOTE_" + str(event.get_index())
+        var note: Node
+        if has_node(note_name):
+            note = get_node(note_name)
+        else:
+            note = Note.instantiate()
+            note.set_name(note_name)
+            add_child(note)
+
+        var pos: Vector2 = event.get_position()
+        pos -= get_viewport().get_visible_rect().get_center()
+
+        var angle: float = -pos.angle_to(Vector2.LEFT)
+
+        angle = (2 * snappedf(angle, TAU/12.) + angle)/3
+        note.get_node("Line2D").set_rotation(angle)
+
+        var pitch: float = pow(2, fmod(angle/TAU, 1))
+        note.get_node("AudioStreamPlayer").set_pitch_scale(pitch)
+
+        if event is InputEventScreenTouch:
+            if event.is_pressed():
+                note.get_node("AnimationPlayer").play("note_on")
+            else:
+                note.get_node("AnimationPlayer").play("note_off")
